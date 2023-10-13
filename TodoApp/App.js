868d35bbcc0b2553c0553f3394fbc5f074e92dea
shahedalazzam <shahed.a.alazzam@gmail.com>
2023-10-13 18:39:19 +0300

    front end is complete

--- TodoApp/App.js
+++ TodoApp/App.js
@@ -1,194 +1,21 @@
-import React, { useEffect, useState } from "react";
-import { View, Text, TouchableOpacity, ScrollView } from "react-native";
-import { Feather } from "@expo/vector-icons";
-import axios from "axios";
-// import TaskList from "./component/TaskList";
-import AddTaskForm from "./src/component/AddTaskForm";
+// App.js
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import TodoList from './component/TodoList';
 
-export default function App() {
-  const [tasks, setTasks] = useState([]);
-  const [darkTheme, setDarkTheme] = useState(false);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    fetchTaskData();
-  }, []);
-
-  const fetchTaskData = async () => {
-    try {
-      const response = await axios.get("https://task-project-c6jd.onrender.com/item/");
-      if (response.data && response.data.data) {
-        setTasks(response.data.data.toDos);
-      }
-    } catch (error) {
-      setError("Error fetching task data. Please try again later.");
-      console.error("Error fetching item data:", error);
-    }
-  };
-
-  const addTask = async (title) => {
-    try {
-      const response = await axios.post('https://task-project-c6jd.onrender.com/item/add', { title });
-      if (response.status === 201) {
-        setTasks(response.data.data.toDos);
-      }
-    } catch (error) {
-      setError("Error creating task. Please try again later.");
-      console.error("Error creating task:", error);
-    }
-  };
-
-  const editTask = async (id, title) => {
-    try {
-      const response = await axios.patch(`https://task-project-c6jd.onrender.com/item/update/${id}`, { title });
-      if (response.data && response.data.data) {
-        setTasks(response.data.data.toDos);
-      }
-    } catch (error) {
-      setError("Error updating task. Please try again later.");
-      console.error("Error updating task:", error);
-    }
-  };
-
-  const deleteTask = async (id) => {
-    try {
-      const response = await axios.delete(`https://task-project-c6jd.onrender.com/item/delete/${id}`);
-      if (response.data && response.data.data) {
-        setTasks(response.data.data.toDos);
-      }
-    } catch (error) {
-      setError("Error deleting task. Please try again later.");
-      console.error("Error deleting task:", error);
-    }
-  };
-
-  const toggleCompleted = async (id) => {
-    try {
-      const response = await axios.patch(`https://task-project-c6jd.onrender.com/item/updatemark/${id}`);
-      if (response.data && response.data.data) {
-        setTasks(response.data.data.toDos);
-      }
-    } catch (error) {
-      setError("Error toggling task completion. Please try again later.");
-      console.error("Error toggling task completion:", error);
-    }
-  };
-  
-  const clearError = () => {
-    setError(null);
-  };
-  
-  const toggleTheme = () => {
-    setDarkTheme((prevTheme) => !prevTheme);
-  };
-  
+const App = () => {
   return (
-    <View
-    style={{
-      flex: 1,
-      backgroundColor: darkTheme ? "#333" : "#fff",
-      alignItems: "center",
-      paddingTop: 20,
-    }}
-    >
-      <View
-        style={{
-          width: "90%",
-          paddingHorizontal: 20,
-          alignItems: "center",
-        }}
-      >
-        <View
-          style={{
-            flexDirection: "row",
-            justifyContent: "space-between",
-            alignItems: "center",
-          }}
-        >
-          <Text
-            style={{
-              fontSize: 30,
-              fontWeight: "bold",
-              color: darkTheme ? "#fff" : "#000",
-            }}
-          >
-            My Tasks
-          </Text>
-          <TouchableOpacity onPress={toggleTheme}>
-            {darkTheme ? (
-              <Feather name="sun" size={32} color="#fff" />
-              ) : (
-                <Feather name="moon" size={32} color="#000" />
-                )}
-          </TouchableOpacity>
-        </View>
-                <AddTaskForm onAddTask={addTask} darkTheme={darkTheme} />
-        <ScrollView
-          style={{
-            width: "100%",
-            backgroundColor: darkTheme ? "#444" : "#fff",
-            maxHeight: 500,
-            borderRadius: 10,
-            shadowColor: "#000",
-            shadowOffset: { width: 0, height: 2 },
-            shadowOpacity: 0.2,
-            shadowRadius: 2,
-            elevation: 3,
-            marginTop: 10,
-          }}
-        >
-          <View
-            style={{
-              flexDirection: "row",
-              justifyContent: "space-between",
-              borderBottomColor: "#ccc",
-              borderBottomWidth: 1,
-              paddingVertical: 10,
-              paddingHorizontal: 15,
-            }}
-          >
-            <Text style={{ color: "#999" }}>
-              {tasks.filter(task => !task.is_completed).length} tasks left
-            </Text>
-          </View>
-          {error && (
-            <View style={{ backgroundColor: "red", padding: 10 }}>
-              <Text style={{ color: "white" }}>{error}</Text>
-              <TouchableOpacity onPress={clearError}>
-                <Text style={{ color: "white" }}>Dismiss</Text>
-              </TouchableOpacity>
-            </View>
-          )}
-          {tasks.map((task) => (
-            <View
-            key={task._id}
-            style={{
-              flexDirection: "row",
-              justifyContent: "space-between",
-              borderBottomColor: "#ccc",
-              borderBottomWidth: 1,
-              paddingVertical: 10,
-              paddingHorizontal: 15,
-            }}
-            >
-              
-              {/* Render task details here */}
-              <Text style={{ color: darkTheme ? "#fff" : "#000" }}>
-                {task.title}
-              </Text>
-              <View style={{ flexDirection: "row", alignItems: "center" }}>
-                <TouchableOpacity onPress={() => toggleCompleted(task.id)}>
-                  {task.is_completed ? (
-                    <Feather name="check-circle" size={24} color="green" />
-                    ) : (
-                      <Feather name="circle" size={24} color="gray" />
-                      )}
-                </TouchableOpacity>
-              </View>
-            </View>
-          ))}
-        </ScrollView>
-      </View>
+    <View style={styles.container}>
+      <TodoList />
     </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+});
+
+export default App;
---
+++ TodoApp/component/TodoList.js
@@ -0,0 +1,173 @@
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  FlatList,
+  StyleSheet,
+  TouchableOpacity,
+  Dimensions,
+} from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
+
+const TodoList = () => {
+  const [tasks, setTasks] = useState([]);
+  const [taskText, setTaskText] = useState("");
+  const [editMode, setEditMode] = useState(false);
+  const [editIndex, setEditIndex] = useState(null);
+  const [editTaskText, setEditTaskText] = useState("");
+
+  const addTask = () => {
+    if (taskText) {
+      setTasks([...tasks, taskText]);
+      setTaskText("");
+    }
+  };
+
+  const removeTask = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks.splice(index, 1);
+    setTasks(updatedTasks);
+  };
+
+  const editTask = (index) => {
+    setEditIndex(index);
+    setEditTaskText(tasks[index]);
+    setEditMode(true);
+  };
+
+  const updateTask = () => {
+    if (editTaskText && editIndex !== null) {
+      const updatedTasks = [...tasks];
+      updatedTasks[editIndex] = editTaskText;
+      setTasks(updatedTasks);
+      setEditIndex(null);
+      setEditTaskText("");
+      setEditMode(false);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.cont}>
+        <TextInput
+          style={styles.input}
+          placeholder="Add a new task ..."
+          value={taskText}
+          onChangeText={(text) => setTaskText(text)}
+        />
+        <TouchableOpacity style={styles.add} onPress={addTask}>
+          <Text>Add</Text>
+        </TouchableOpacity>
+      </View>
+      <FlatList
+        data={tasks}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item, index }) => (
+          <View style={styles.taskItem}>
+            {editIndex === index && editMode ? (
+              <View style={styles.update}>
+                <TextInput
+                  style={styles.upinput}
+                  value={editTaskText}
+                  onChangeText={(text) => setEditTaskText(text)}
+                />
+                <TouchableOpacity style={styles.button} onPress={updateTask}>
+                  <Icon name="check" size={23} color="#9F45FF" />
+                </TouchableOpacity>
+              </View>
+            ) : (
+              <View style={styles.taskText}>
+                <Text style={styles.edinput}>{item}</Text>
+                <View style={styles.buttons}>
+                  <TouchableOpacity
+                    style={styles.button}
+                    onPress={() => editTask(index)}
+                  >
+                    <Icon name="edit" size={22} color="#9F45FF" />
+                  </TouchableOpacity>
+                  <TouchableOpacity
+                    style={styles.button}
+                    onPress={() => removeTask(index)}
+                  >
+                    <Icon name="trash" size={22} color="#9F45FF" />
+                  </TouchableOpacity>
+                </View>
+              </View>
+            )}
+          </View>
+        )}
+      />
+    </View>
+  );
+};
+
+const windowWidth = Dimensions.get("window").width;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 12,
+  },
+  input: {
+    width: windowWidth * 0.666,
+    borderColor: "gray",
+    borderWidth: 1,
+    paddingHorizontal: 8,
+  },
+  taskItem: {
+    paddingLeft: 5,
+    borderWidth: 1,
+    borderColor: "#9F45FF",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginTop: 10,
+  },
+  taskText: {
+    marginBottom: 10,
+    marginTop: 10,
+    width: windowWidth * 0.53,
+    justifyContent: "space-between",
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  button: {
+    backgroundColor: "#fff",
+    padding: 5,
+    borderRadius: 5,
+  },
+  buttons: {
+    flexDirection: "row",
+  },
+  add: {
+    alignItems: "center",
+    borderWidth: 2,
+    borderColor: "#9F45FF",
+    width: windowWidth * 0.2,
+  },
+  cont: {
+    marginTop: 50,
+    height: 40,
+    marginBottom: 10,
+    justifyContent: "space-between",
+    flexDirection: "row",
+  },
+  update: {
+    height: 53,
+    paddingLeft: 5,
+    alignItems: "center",
+    flexDirection: "row",
+  },
+  upinput: {
+    paddingRight: 15,
+    width: windowWidth * 0.7,
+  },
+  edinput: {
+    paddingLeft: 5,
+    paddingRight: 15,
+    width: windowWidth * 0.67,
+  },
+});
+
+export default TodoList;
